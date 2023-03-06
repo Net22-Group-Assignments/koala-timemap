e1a7d2dfaf07fd4da6f0477a48c8eba7441447a3
Björn Agnemo <bjorn.agnemo@gmail.com>
2023-03-06 09:27:30 +0100

    stash away

--- client/src/components/AuthProvider.js
+++ client/src/components/AuthProvider.js
@@ -3,19 +3,32 @@ import { createContext, useContext, useEffect, useState } from "react";
 const AuthContext = createContext();
 export const useAuth = () => useContext(AuthContext);
 export const AuthProvider = ({ children }) => {
-  const [clientId, setClientId] = useState();
+  const [clientId, setClientId] = useState("");
   const [authed, setAuthed] = useState(false);
-  const [user, setUser] = useState({});
+  //const [user, setUser] = useState({});
 
-  useEffect(() => {
+  useEffect(async () => {
+    const status = await fetch("/status")
+        .then((res) => res.json())
+        .then((data) => data.message);
+    if (status) {
+      setAuthed(status);
+      return;
+    }
+
     fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setClientId(data.message));
+      .then((res) => res.text())
+      .then((data) => setClientId(data));
   });
   const authorize = async (code) => {
     //fetch here
-    fetch(`/login/${code}`)
-      .then((res) => res.json())
+    await fetch(`/login`, {
+      method: "POST",
+          headers: {
+      "Content-Type": "text/plain",
+      body: code
+    });
+        .then((res) => res.json())
       .then((data) => {
         setUser(data.message);
         console.log(user);
--- server/server.js
+++ server/server.js
@@ -1,37 +1,90 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const axios = require("axios");
+const storage = require("node-persist");
 
 dotenv.config();
+
+let access_token = null;
+
+(async function () {
+  await storage.init();
+  access_token = storage.getItem("access_token");
+})();
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(express.text());
 
-app.get("/api", (req, res) => {
-  res.json({ message: process.env.NOTION_OAUTH_CLIENT_ID });
+const status = { tokenRegistered: access_token !== null };
+
+app.get("/status", (req, res) => {
+  res.json({ status });
+});
+app.get("/clientId", (req, res) => {
+  res.status(200).send(process.env.NOTION_OAUTH_CLIENT_ID);
 });
 
-app.get("/login/:code", async (req, res) => {
-  const { code } = req.params;
+app.get("/accessToken", (req, res) => {
+  res.status(200).send(access_token);
+});
+
+app.post("/login", async (req, res) => {
+  const code = req.body;
   console.log(`code: ${code}`);
   const options = {
     method: "POST",
     url: "https://api.notion.com/v1/oauth/token",
     auth: {
-      username: "dd6fae9a-921b-45b5-ab34-92e85932a89f",
-      password: "secret_oUli5Gq6yc5cEHrEBfC1vj8HIAs0tCI5LndIoQXLtEX",
+      username: process.env.NOTION_OAUTH_CLIENT_ID,
+      password: process.env.NOTION_OAUTH_CLIENT_SECRET,
     },
-    headers: { Accept: "application/json", "Content-type": "application/json" },
+    headers: { "Content-type": "application/json" },
     data: {
-      code: code,
       grant_type: "authorization_code",
-      redirect_uri: "http://localhost:3000",
+      code: code,
     },
   };
+  console.log(options);
+  let data = null;
+  try {
+    const response = await axios.request(options);
+    data = response.data;
+  } catch (error) {
+    //console.log(error);
+    if (data?.hasOwnProperty("error")) {
+      res.status(500).send(data.error);
+      return;
+    }
+    res.status(500).send("Major Malfunction with code " + code);
+    return;
+  }
 
-  const response = await axios.request(options);
-  res.json({ message: response.data.owner });
+  access_token = data.access_token;
+  await storage.setItem("access_token", access_token);
+  status.tokenRegistered = true;
+  console.log(`access_token ${access_token}`);
+  res.json({ message: data.owner });
+  //res.status(200).send(`accesstoken: ${access_token}`);
+});
+
+app.post("/testLogin", async (req, res) => {
+  const code = req.body;
+  console.log(`code: ${code}`);
+  access_token = code;
+  await storage.setItem("access_token", access_token);
+  status.tokenRegistered = true;
+  console.log(`access_token ${access_token}`);
+  //res.json({ message: response.data.owner });
+  res.status(200).send(`accesstoken: ${access_token}`);
+});
+
+app.post("/logout", async (req, res) => {
+  access_token = null;
+  await storage.removeItem("access_token");
+  status.tokenRegistered = false;
+  res.status(200).send("Accesstoken removed");
 });
 
 app.listen(PORT, () => {
