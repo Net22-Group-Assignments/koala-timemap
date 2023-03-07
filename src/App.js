228661b14d9f653c73975f88e045b4fd5a608544
Jon Westman <jon.westman@edu.edugrade.se>
2023-03-07 13:59:00 +0100

    updated navbar with navlink and some light design to make it user friendly

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 
 import { BrowserRouter as Router, HashRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar';
+import Projects from './pages/Project';
+import TimeRegistry from './pages/TimeRegistry';
+import TimeSummary from './pages/TimeSummary';
 
 function App() {
   return (
-  <Router>
-    <Navbar>
-      <Routes>
-        <Route path='' element='' />
-      </Routes>
+    <Router>
+
+      <Navbar>
+        <Routes>
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/timeregistry' element={<TimeRegistry />} />
+          <Route path='/timesummary' element={<TimeSummary />} />
+        </Routes>
       </Navbar>
     </Router>
+
   );
 }
 
---
+++ src/pages/TimeRegistry.js
@@ -0,0 +1,5 @@
+
+
+export default function TimeRegistry(){
+    return <p>Time Registry go here</p>
+}
