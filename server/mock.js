7016be5c2e04e7ad7ddb8fe485f94f0b9a066be3
Björn Agnemo <bjorn.agnemo@gmail.com>
2023-03-08 11:05:53 +0100

    refactor: remove commented code

--- server/mock.js
+++ server/mock.js
@@ -1,11 +1,6 @@
 const axios = require("axios");
 const dotenv = require("dotenv");
 
-// dotenv.config();
-//
-// const apiURL = process.env.NOTION_API_URL;
-// const apiVersion = process.env.NOTION_API_VERSION;
-
 const MockClient = {
   users: {
     me() {
@@ -23,53 +18,5 @@ const MockClient = {
     },
   },
 };
-
-// Returns user object associated with current access_token
-// const getMe = async (accessToken) => {
-//   try {
-//     const response = await axios.get(`${apiURL}/users/me`, {
-//       headers: {
-//         Authorization: `Bearer ${accessToken}`,
-//         "Notion-Version": apiVersion,
-//       },
-//     });
-//     return response.data.bot.owner;
-//   } catch (error) {
-//     console.error(error.response.status);
-//   }
-// };
-//
-// // Returns user object associated with userId
-// const getUser = async (accessToken, userId) => {
-//   try {
-//     const response = await axios.get(`${apiURL}/users/${userId}`, {
-//       headers: {
-//         Authorization: `Bearer ${accessToken}`,
-//         "Notion-Version": apiVersion,
-//       },
-//     });
-//     return response.data;
-//   } catch (error) {
-//     console.error(error.response.status);
-//   }
-// };
-//
-// const getUsers = async (accessToken) => {
-//   try {
-//     const response = await axios.get(`${apiURL}/users/`, {
-//       headers: {
-//         Authorization: `Bearer ${accessToken}`,
-//         "Notion-Version": apiVersion,
-//       },
-//     });
-//     return response.data;
-//   } catch (error) {
-//     console.error(error.response.status);
-//   }
-// };
-//
-// exports.getMe = getMe;
-// exports.getUser = getUser;
-// exports.getUsers = getUsers;
 
 exports.MockClient = MockClient;
