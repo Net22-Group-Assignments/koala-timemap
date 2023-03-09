4c78ac0064f085eada9361635fc6155fdc215640
Björn Agnemo <bjorn.agnemo@gmail.com>
2023-03-09 17:33:15 +0100

    feat: add beginning of component to show timesummary

--- client/src/components/GetTime.js
+++ client/src/components/GetTime.js
@@ -1,14 +1,24 @@
+import { useEffect, useState } from "react";
+
 export default function GetTime() {
+  const [projectData, setProjectData] = useState(null);
+  useEffect(() => {
+    fetch("/projects")
+      .then((res) => res.json())
+      .then((data) => setProjectData(data));
+  }, []);
+
   return (
     <div>
       <ul>
-        {/*<li><span>{projectName}</span><span>{projectStatus}</span><span>{hours}</span><span>{workedHours}</span></li>*/}
-        <li>
-          <span>My first project</span>
-          <span>Done</span>
-          <span>10</span>
-          <span>4</span>
-        </li>
+        {projectData &&
+          projectData.results.map((project) => (
+            <li>
+              <span>
+                {project.properties.Projectname.title[0].text.content}
+              </span>
+            </li>
+          ))}
       </ul>
     </div>
   );
