060b8428d16385656e6cd336d3edde4e35768817
oskar åhling <oskar.ahling@edu.edugrade.se>
2023-03-13 18:00:49 +0100

    Done with getTime for the moment. will need to add more functions later.

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/NavBar-folder/Navbar";
 import Projects from "./pages/Project";
 import TimeRegistry from "./pages/TimeRegistry";
-import TimeSummary from "./pages/TimeSummary";
+import TimeSummary from "./pages/TimePage/TimeSummary";
 import Dev from "./pages/Dev";
 
 function App() {
