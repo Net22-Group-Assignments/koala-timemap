8e39e3c220ccc71bd3b83c5bb266f12c2b0bddb2
Björn Agnemo <bjorn.agnemo@gmail.com>
2023-03-16 14:58:19 +0100

    feat(server): add routes and service for login
    This is only a placeholder for further work. Frontend development can continue without further additions if necessary.

--- server/routes/people_routes.js
+++ server/routes/people_routes.js
@@ -3,14 +3,14 @@ const PeopleService = require("../service/people_service");
 const router = express.Router();
 
 router.get("/people", async (req, res) => {
-  const { simplify } = req.query;
-  res.json(await PeopleService.getPeople(null, simplify));
+  const { schema } = req.query;
+  res.json(await PeopleService.getPeople(null, schema));
 });
 
 router.get("/people/:peopleId", async (req, res) => {
   const { peopleId } = req.params;
-  const { simplify } = req.query;
-  res.json(await PeopleService.getPeopleById(peopleId, simplify));
+  const { schema } = req.query;
+  res.json(await PeopleService.getPeopleById(peopleId, schema));
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const UserService = require("./service/user_service");
 const ProjectsService = require("./service/projects_service");
 const TimeReportsService = require("./service/timereports_service");
 const PeopleService = require("./service/people_service");
+const LoginService = require("./service/login_service");
 const integrationArgIndex = process.argv.indexOf("--integration");
 
 dotenv.config();
@@ -22,6 +23,7 @@ let status = {
 
 if (integrationArgIndex > -1) {
   status.integration_type = process.argv[integrationArgIndex + 1];
+  process.env.INTEGRATION_TYPE = "internal";
 }
 
 (async () => {
@@ -31,18 +33,19 @@ if (integrationArgIndex > -1) {
   }
   status.access_token = process.env.NOTION_API_KEY;
   console.log(`Access token: ${status.access_token}`);
-  Notion.configure(status.integration_type, status.access_token);
-  UserService.configure(Notion);
-  try {
-    console.log(await UserService.getTokenBotUser());
-    console.log("Valid internal token");
-    status.valid_token = true;
-  } catch (e) {
-    console.error(e);
+  status.valid_token = await Notion.configure(
+    status.integration_type,
+    status.access_token
+  );
+  if (!status.valid_token) {
+    console.error("Invalid internal token");
   }
+  console.log("Valid internal token");
+  UserService.configure(Notion);
   ProjectsService.configure(Notion);
   TimeReportsService.configure(Notion);
   PeopleService.configure(Notion);
+  LoginService.configure(Notion);
 })();
 
 const PORT = 3001;
@@ -61,6 +64,7 @@ app.use("/api", require("./routes/users_routes"));
 app.use("/api", require("./routes/people_routes"));
 app.use("/api", require("./routes/timereports_routes"));
 app.use("/api", require("./routes/projects_routes"));
+app.use("/api", require("./routes/login_routes"));
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
@@ -82,72 +86,3 @@ app.get("/api/clientId", (req, res) => {
 app.get("/api/accessToken", (req, res) => {
   res.json({ accessToken: status.access_token });
 });
-
-// app.post("/login", async (req, res) => {
-//   if (integrationType !== "public") {
-//     res.status(400).send("Running with internal access token");
-//     return;
-//   }
-//   if (validToken) {
-//     res.status(400).send("Accesstoken already registered and validated");
-//     return;
-//   }
-//
-//   const code = req.body.code;
-//   console.log(`code: ${code}`);
-//   const options = {
-//     method: "POST",
-//     url: "https://api.notion.com/v1/oauth/token",
-//     auth: {
-//       username: process.env.NOTION_OAUTH_CLIENT_ID,
-//       password: process.env.NOTION_OAUTH_CLIENT_SECRET,
-//     },
-//     headers: { "Content-type": "application/json" },
-//     data: {
-//       grant_type: "authorization_code",
-//       code: code,
-//     },
-//   };
-//   console.log(options);
-//   let data = null;
-//   try {
-//     const response = await axios.request(options);
-//     data = response.data;
-//     accessToken = data.access_token;
-//     notion = new Client({
-//       auth: accessToken,
-//     });
-//
-//     try {
-//       const user = await notion.users.me();
-//       validToken = true;
-//       console.log(`USER: ${user}`);
-//     } catch (error) {
-//       console.error(error);
-//     }
-//     await storage.setItem("access_token", accessToken);
-//     console.log(`access_token ${accessToken}`);
-//
-//     res.status(200).end();
-//   } catch (error) {
-//     console.error(error.status);
-//     if (data?.hasOwnProperty("error")) {
-//       res.status(500).send(data.error);
-//       return;
-//     }
-//     res.status(500).end();
-//   }
-// });
-//
-// app.post("/logout", async (req, res) => {
-//   if (integrationType !== "public") {
-//     res.status(400).send("Running with internal access token");
-//     return;
-//   }
-//   accessToken = null;
-//   validToken = false;
-//   notion = new Client();
-//   await storage.removeItem("access_token");
-//   console.log("Accesstoken removed");
-//   res.status(200).send("Accesstoken removed");
-// });
