c76efd277c0ad2e24c379b6984d4c8fa2f0f1b0b
Björn Agnemo <bjorn.agnemo@gmail.com>
2023-03-16 15:05:56 +0100

    feat(client): add login developer mode
    When running on internal integration "god mode" is activated and anyone can be everyone. Depends on https://authkit.arkadip.dev/
    
    BREAKING CHANGE: Client is even more tigthly tied to loginservice

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,50 @@ import Projects from "./pages/Project";
 import TimeRegistry from "./pages/TimeRegistry";
 import TimeSummary from "./pages/TimePage/TimeSummary";
 import Dev from "./pages/Dev";
+import { useAuthUser, useSignIn, useIsAuthenticated } from "react-auth-kit";
+import { useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const signIn = useSignIn();
+  const auth = useAuthUser();
+  const isAuthenticated = useIsAuthenticated();
+
+  useEffect(() => {
+    if (
+      process.env.NODE_ENV !== "production" &&
+      process.env.REACT_APP_INTEGRATION_TYPE === "internal"
+    ) {
+      if (!isAuthenticated()) {
+        axios.post("/api/login").then((res) => {
+          if (res.status === 200) {
+            if (
+              signIn({
+                token: res.data.token,
+                expiresIn: res.data.expiresIn,
+                tokenType: "string",
+                authState: res.data.authState,
+              })
+            ) {
+              console.log("Login success");
+              console.log(auth());
+            } else {
+              console.error("Login failed");
+            }
+          }
+        });
+      }
+    }
+  }, [isAuthenticated]);
+
   return (
     <>
       <Navbar />
+      {isAuthenticated() && (
+        <p>
+          Current User: {auth().person.name} Current Role: {auth().person.role}
+        </p>
+      )}
       <Routes>
         <Route path="/" element={<TimeSummary />} />
         <Route path="/Timesummary" element={<TimeSummary />} />
--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
+import { AuthProvider } from "react-auth-kit";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <AuthProvider authType="localstorage" authName="_auth">
+      <Router>
+        <App />
+      </Router>
+    </AuthProvider>
   </React.StrictMode>
 );
 
