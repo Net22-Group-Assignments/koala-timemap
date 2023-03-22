305a0959dd87fa943c0c000e23e303021c515473
jonwestman <jonwestman88@gmail.com>
2023-03-22 18:50:47 +0100

    removed x:es from Modals

--- client/src/pages/TimePage/EditProject.js
+++ client/src/pages/TimePage/EditProject.js
@@ -30,7 +30,7 @@ export default function EditProject(props) {
         backdrop="static"
         keyboard={false}
       >
-        <Modal.Header closeButton>
+        <Modal.Header>
           <Modal.Title>Add Project</Modal.Title>
         </Modal.Header>
         <Modal.Body>
