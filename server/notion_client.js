d61a6419416e9d263784fce059fada3d5299e54a
Björn Agnemo <bjorn.agnemo@gmail.com>
2023-03-23 09:31:20 +0100

    refactor(server): remove commented code for notion notion client

--- server/notion_client.js
+++ server/notion_client.js
@@ -1,38 +1,5 @@
 const { Client, LogLevel } = require("@notionhq/client");
 
-// const Notion = {
-//   token: null,
-//   client: null,
-//   integration: {
-//     id: null,
-//     type: null,
-//     name: null,
-//   },
-//   configure: async function (integrationType, token) {
-//     if (token != null) {
-//       const newClient = new Client({
-//         auth: token,
-//         logLevel: LogLevel.DEBUG,
-//       });
-//       try {
-//         const response = await newClient.users.me();
-//         this.token = token;
-//         this.client = newClient;
-//         this.integration.id = response.id;
-//         this.integration.type = integrationType;
-//         this.integration.name = response.name;
-//         return true;
-//       } catch (e) {
-//         console.error(e);
-//         return false;
-//       }
-//     }
-//   },
-// };
-
-// Factory returns a ClientPool object that can be used to obtain a Notion client
-// If a database is passed in, the ClientPool will be configured to use the
-// database to store and retrieve tokens for public integrations
 const ClientPoolFactory = (db = null) => {
   if (!db) {
     return InternalClientPool(process.env.NOTION_API_KEY);
