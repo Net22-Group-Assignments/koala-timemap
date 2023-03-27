4daa761493fa54d78c2c39db3a8d43712517e34c
oskar åhling <oskar.ahling@edu.edugrade.se>
2023-03-27 14:51:11 +0200

    ColorConfig to Timesummary done

--- client/src/pages/TimePage/TimeSummary.js
+++ client/src/pages/TimePage/TimeSummary.js
@@ -67,27 +67,6 @@ export default function TimeSummary() {
       });
   }
 
-  useEffect(() => {
-    projects &&
-      projects.map((project) => {
-        if (parseInt(project.properties.HoursLeft.formula.number) < 0) {
-          console.log(
-            project.properties.HoursLeft.formula.number,
-            "hours left minus"
-          );
-          setCheckTime("lightpink");
-          console.log(checkTime, "red");
-        } else if (parseInt(project.properties.HoursLeft.formula.number) > 0) {
-          console.log(
-            project.properties.HoursLeft.formula.number,
-            "hours left plus"
-          );
-          setCheckTime("lightgreen");
-          console.log(checkTime, "lightgreen");
-        }
-      });
-  });
-
   return (
     <div className="Table_container m-2">
       <div className="flex justify-content: flex-end">
@@ -123,7 +102,14 @@ export default function TimeSummary() {
             ? projects.map((project) => (
                 <tbody>
                   <tr>
-                    <td style={{ backgroundColor: checkTime }}>
+                    <td
+                      style={{
+                        backgroundColor:
+                          project.properties.HoursLeft.formula.number < 0
+                            ? "lightpink"
+                            : "lightgreen",
+                      }}
+                    >
                       {project.properties.Projectname.title[0].text.content}
                     </td>
                     <td>{project.properties.Status.select.name}</td>
