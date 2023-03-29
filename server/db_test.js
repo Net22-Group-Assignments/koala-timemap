498f5fd58a9a4561c6097b2a1302e31115889833
Björn Agnemo <bjorn.agnemo@gmail.com>
2023-03-29 09:47:25 +0200

    removed unneccesary file

--- server/db_test.js
+++
@@ -1,8 +0,0 @@
-const db = require("./db");
-
-(async () => {
-  // const tokens = await db.getAllTokens();
-  // console.log(tokens);
-  const token = await db.getToken("16eb2615-67cb-49af-8adb-8f9036cc9144");
-  console.log(token);
-})();
