3abeba9114858bd9950237618b753b3d296b661c
jonwestman <jonwestman88@gmail.com>
2023-04-02 10:19:45 +0200

    minor updates to boss view

---
+++ client/src/pages/TimePage/BossView.js
@@ -0,0 +1,12 @@
+import Card from "react-bootstrap/Card";
+import BodyOnly from "../../components/Cards/SingleCard";
+import MultiCard from "../../components/Cards/MultipleCard";
+
+export default function ManagerView() {
+  return (
+    <>
+      <BodyOnly />
+      <MultiCard />
+    </>
+  );
+}
