f950ae57189c8daf2ae60a900b6c66997d39b523
oskar åhling <112972443+EduOskar@users.noreply.github.com>
2023-04-02 13:35:38 +0200

    text-editing added

--- client/src/pages/TimePage/TimeSummary.js
+++ client/src/pages/TimePage/TimeSummary.js
@@ -169,128 +169,137 @@ export default function TimeSummary(props) {
         </div>
       </div>
       {/* Here is display for projects DB */}
-      <div className="Project_container">
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Project Name</th>
-              <th>Status</th>
-              <th>Hours</th>
-              <th>Estimated hours left</th>
-              <th>Worked Hours</th>
-              <th>Start-Date</th>
-              <th>End-Date</th>
-            </tr>
-          </thead>
-          {projects && timeData
-            ? getFilteredProjects()
-                .filter((status) =>
-                  status.properties.Status.select.name.includes(
-                    SelectedRadioBtn
+      <div className="project_container_container">
+        <p>Project</p>
+        <div className="Project_container">
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Project Name</th>
+                <th>Status</th>
+                <th>Hours</th>
+                <th>Estimated hours left</th>
+                <th>Worked Hours</th>
+                <th>Start-Date</th>
+                <th>End-Date</th>
+              </tr>
+            </thead>
+            {projects && timeData
+              ? getFilteredProjects()
+                  .filter((status) =>
+                    status.properties.Status.select.name.includes(
+                      SelectedRadioBtn
+                    )
                   )
-                )
-                .map((project) => (
+                  .map((project) => (
+                    <tbody>
+                      <tr>
+                        <td
+                          className="project_status"
+                          id={project.properties.Status.select.name}
+                          style={{
+                            backgroundColor:
+                              project.properties.Status.select.name === "Active"
+                                ? "yellow"
+                                : "",
+                          }}
+                        >
+                          {project.properties.Projectname.title[0].text.content}
+                          {props.children}
+                        </td>
+                        <td>{project.properties.Status.select.name}</td>
+                        <td>{project.properties.Hours.number}</td>
+                        <td
+                          style={{
+                            backgroundColor:
+                              project.properties.HoursLeft.formula.number < 0
+                                ? "lightpink"
+                                : "",
+                          }}
+                          value={
+                            project.properties.HoursLeft.formula.number < 0
+                              ? (timeProject = "WARNING")
+                              : (timeProject = "")
+                          }
+                        >
+                          {project.properties.HoursLeft.formula.number}{" "}
+                          {timeProject}
+                        </td>
+                        <td>{project.properties.WorkedHours.rollup.number}</td>
+                        <td>{project.properties.Timespan.date.start}</td>
+                        <td>{project.properties.Timespan.date.end}</td>
+                      </tr>
+                    </tbody>
+                  ))
+              : null}
+          </Table>
+        </div>
+      </div>
+      {/* Here is the display for People DB */}
+      <div className="people_container_container">
+        <p>People Timereport</p>
+        <div className="People_container">
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Names</th>
+                <th>Total hours worked </th>
+                <th>Role</th>
+              </tr>
+            </thead>
+            {peopleData
+              ? peopleData.results.map((people) => (
                   <tbody>
                     <tr>
-                      <td
-                        className="project_status"
-                        id={project.properties.Status.select.name}
-                        style={{
-                          backgroundColor:
-                            project.properties.Status.select.name === "Active"
-                              ? "yellow"
-                              : "",
-                        }}
-                      >
-                        {project.properties.Projectname.title[0].text.content}
-                        {props.children}
-                      </td>
-                      <td>{project.properties.Status.select.name}</td>
-                      <td>{project.properties.Hours.number}</td>
-                      <td
-                        style={{
-                          backgroundColor:
-                            project.properties.HoursLeft.formula.number < 0
-                              ? "lightpink"
-                              : "",
-                        }}
-                        value={
-                          project.properties.HoursLeft.formula.number < 0
-                            ? (timeProject = "WARNING")
-                            : (timeProject = "")
-                        }
-                      >
-                        {project.properties.HoursLeft.formula.number}{" "}
-                        {timeProject}
-                      </td>
-                      <td>{project.properties.WorkedHours.rollup.number}</td>
-                      <td>{project.properties.Timespan.date.start}</td>
-                      <td>{project.properties.Timespan.date.end}</td>
+                      <td>{people.properties.Name.title[0].text.content}</td>
+                      <td>{people.properties.TotalHours.rollup.number}</td>
+                      <td>{people.properties.Role.select.name}</td>
                     </tr>
                   </tbody>
                 ))
-            : null}
-        </Table>
-      </div>
-      {/* Here is the display for People DB */}
-      <div className="People_container">
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Names</th>
-              <th>Total hours worked </th>
-              <th>Role</th>
-            </tr>
-          </thead>
-          {peopleData
-            ? peopleData.results.map((people) => (
-                <tbody>
-                  <tr>
-                    <td>{people.properties.Name.title[0].text.content}</td>
-                    <td>{people.properties.TotalHours.rollup.number}</td>
-                    <td>{people.properties.Role.select.name}</td>
-                  </tr>
-                </tbody>
-              ))
-            : null}
-        </Table>
+              : null}
+          </Table>
+        </div>
       </div>
       {/* Here is the display for TimeProject DB */}
-      <div className="TimeReport_container">
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Person</th>
-              <th>Hours</th>
-              <th>Project</th>
-              <th>Note</th>
-            </tr>
-          </thead>
-          {timeData
-            ? timeData.results.map((time) => (
-                <tbody>
-                  <tr>
-                    <td>{time.properties.Date.date.start}</td>
-                    <td>
-                      {
-                        time.properties.Person.relation_properties.Name.title[0]
-                          .text.content
-                      }
-                    </td>
-                    <td>{time.properties.Hours.number.text}</td>
-                    <td>
-                      {
-                        time.properties.Project.relation_properties.Projectname
-                          .title[0].text.content
-                      }
-                    </td>
-                    {<td>{time.properties.Note.title[0].text.content}</td>}
-                  </tr>
-                </tbody>
-              ))
-            : null}
-        </Table>
+      <div className="timereport_container_container">
+        <p>TimeReport</p>
+        <div className="TimeReport_container">
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Person</th>
+                <th>Hours</th>
+                <th>Project</th>
+                <th>Note</th>
+              </tr>
+            </thead>
+            {timeData
+              ? timeData.results.map((time) => (
+                  <tbody>
+                    <tr>
+                      <td>{time.properties.Date.date.start}</td>
+                      <td>
+                        {
+                          time.properties.Person.relation_properties.Name
+                            .title[0].text.content
+                        }
+                      </td>
+                      <td>{time.properties.Hours.number.text}</td>
+                      <td>
+                        {
+                          time.properties.Project.relation_properties
+                            .Projectname.title[0].text.content
+                        }
+                      </td>
+                      {<td>{time.properties.Note.title[0].text.content}</td>}
+                    </tr>
+                  </tbody>
+                ))
+              : null}
+          </Table>
+        </div>
       </div>
     </div>
   );
