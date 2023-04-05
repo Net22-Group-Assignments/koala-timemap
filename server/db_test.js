3cc501e75eeec7531d439ba771323b04e5a131ac
Björn Agnemo <bjorn.agnemo@gmail.com>
2023-04-05 14:17:07 +0200

    Cleanup in progress

--- server/db_test.js
+++
@@ -1,9 +0,0 @@
-const dbRunner = require("./db.js");
-const otpService = require("./service/otp_service.js");
-
-(async () => {
-  // const otpGenerated = await otpService.generateOtp("123");
-  // console.log("otpGenerated: " + otpGenerated);
-  const otpVerified = await otpService.verifyOtp("123", "448883");
-  console.log("otpVerified: " + otpVerified);
-})();
--- server/sandbox/otp_test.js
+++
@@ -1,8 +0,0 @@
-const OTP = require("one-time-password");
-
-const dummyKey = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
-
-// const dummyToken = OTP.generate(dummyKey);
-// console.log(dummyToken);
-console.log(OTP.verify(dummyKey, "448883"));
-console.log(OTP.verify(dummyKey, dummyToken));
